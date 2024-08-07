342fef7fbe9d940a74892033094f7326cfe727be
Waffy_Sugar <43415202+stevenxfshen@users.noreply.github.com>
2024-08-07 01:41:36 -0400

    Update installnodeexporter.sh

--- installnodeexporter.sh
+++ installnodeexporter.sh
@@ -1,6 +1,18 @@
+sudo apt install software-properties-common
 sudo apt update
-sudo apt install golang-go
+sudo apt install golang-go -y
+sudo apt install make
 git clone https://github.com/prometheus/node_exporter.git
 cd node_exporter
 make build
-./node_exporter <flags>
+cp node_exporter /usr/bin
+
+
+
+sudo groupadd -f node_exporter
+sudo useradd -g node_exporter --no-create-home --shell /bin/false node_exporter
+sudo mkdir /etc/node_exporter
+sudo chown node_exporter:node_exporter /etc/node_exporter
+sudo chown node_exporter:node_exporter /usr/bin/node_exporter
+
+
