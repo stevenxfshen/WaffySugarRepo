48e9736d8e60f449d5e12f9e30bccae710cc9dff
Steven <43415202+stevenxfshen@users.noreply.github.com>
2025-01-14 04:24:47 -0500

    update the ip_forward
    update the ip_forward

--- install5gs_ubuntu_build.sh
+++ install5gs_ubuntu_build.sh
@@ -4,12 +4,14 @@
 sudo apt update
 sudo apt upgrade -y
 
-# Install gnupg and curl
+# Install gnupg and curl and meson git
 sudo apt install gnupg -y
 sudo apt install curl -y
+sudo apt install meson  -y
+sudo apt install git -y
 
 # Install MongoDB
-curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg --dearmor
+curl -fsSL https://pgp.mongodb.com/server-6.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg --dearmor
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 sudo apt update
 sudo apt install -y mongodb-org
@@ -17,9 +19,18 @@ sudo systemctl start mongod
 sudo systemctl enable mongod
 
 # Install Open5GS
-sudo add-apt-repository ppa:open5gs/latest -y
-sudo apt update
-sudo apt install open5gs -y
+sudo apt install python3-pip python3-setuptools python3-wheel ninja-build build-essential flex bison git cmake libsctp-dev libgnutls28-dev libgcrypt-dev libssl-dev libidn11-dev libmongoc-dev libbson-dev libyaml-dev libnghttp2-dev libmicrohttpd-dev libcurl4-gnutls-dev libnghttp2-dev libtins-dev libtalloc-dev meson -y
+git clone https://github.com/open5gs/open5gs
+cd open5gs
+meson build --prefix=`pwd`/install
+ninja -C build
+./build/tests/registration/registration
+#cd build
+#meson test -v
+
+cd build
+ninja install
+cd ../
 
 # Install Node.js
 sudo apt update
@@ -31,6 +42,10 @@ echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.co
 sudo apt update
 sudo apt install nodejs -y
 
+cd webui
+npm ci
+npm run dev
+
 # Install Open5GS WebUI
 curl -fsSL https://open5gs.org/open5gs/assets/webui/install | sudo -E bash -
 sudo sed -i '/Environment=NODE_ENV=production/a Environment=HOSTNAME=0.0.0.0\nEnvironment=PORT=9999' /lib/systemd/system/open5gs-webui.service
@@ -41,6 +56,8 @@ sudo ip addr add 10.45.0.1/16 dev ogstun
 sudo ip link set ogstun up
 
 sudo sysctl -w net.ipv4.ip_forward=1
+#sudo nano /etc/sysctl.conf
+#net.ipv4.ip_forward = 1
 sudo iptables -t nat -A POSTROUTING -s 10.45.0.0/16 ! -o ogstun -j MASQUERADE
 sudo iptables -I INPUT -i ogstun -j ACCEPT
 sudo iptables -I INPUT -s 10.45.0.0/16 -j DROP
